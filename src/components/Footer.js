0d623f8f0e7406e428feea64604cd9b8ba3d221f
Mickey <mickey.zheng@yahoo.com>
2021-10-20 00:04:22 -0400

    Added basic Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
-    return <div>
-        This is the footer
-    </div>
+    const githubLink = 'https://github.com/mwzheng/portfolio-react';
+
+    return <footer className='fixed-bottom py-3 text-center bg-dark text-white'>
+        <div className='container '>
+            <p className='lead'>
+                Copyright &copy; 2021&emsp;|&emsp;
+                <i className='bi bi-github'> </i>
+                <a href={githubLink} target='_blank' rel='noreferrer'>GitHub</a>&emsp;|&emsp;
+                <Link to='/'>Home</Link>&emsp;|&emsp;
+                <Link to='/resume'>Resume</Link>&emsp;|&emsp;
+                <Link to='/projects' >Projects</Link>
+            </p>
+        </div>
+    </footer>
 }
 
 export default Footer;
