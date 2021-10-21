ed8591f3d1ac5f282251bd852c94132980447681
Mickey <mickey.zheng@yahoo.com>
2021-10-21 17:17:56 -0400

    Finished basic Projects page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import allProjects from '../allProjects.json';
+import ProjectCard from './ProjectCard';
 
 const Projects = () => {
-    return <div>
-        This is my Projects component
+    const [projectList, setProjectList] = useState(allProjects);
+
+    const createProjectCards = () => {
+        return projectList.map(project => {
+            return <ProjectCard key={project.projectName} project={project} />
+        })
+    }
+
+    return <div className='m-3 d-flex flex-wrap justify-content-around'>
+        {createProjectCards()}
     </div>
 }
 
