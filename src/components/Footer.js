42fffc11a8c71a43510e64bbedc559727a2caf68
Mickey <mickey.zheng@yahoo.com>
2021-10-21 18:11:39 -0400

    Added LinkedIn to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
     const githubLink = 'https://github.com/mwzheng';
+    const linkedinLink = 'https://www.linkedin.com/in/mickey-zheng-a35a521aa/';
 
     return <footer className='navbar fixed-bottom bg-dark text-white'>
         <div className='container'>
@@ -10,6 +11,8 @@ const Footer = () => {
                 Copyright &copy; 2021&emsp;|&emsp;
                 <i className='bi bi-github'> </i>
                 <a href={githubLink} target='_blank' rel='noreferrer'>GitHub</a>&emsp;|&emsp;
+                <i class="bi bi-linkedin"> </i>
+                <a href={linkedinLink} target='_blank' rel='noreferrer'>LinkedIn</a>&emsp;|&emsp;
                 <Link to='/'>Home</Link>&emsp;|&emsp;
                 <Link to='/resume'>Resume</Link>&emsp;|&emsp;
                 <Link to='/projects' >Projects</Link>
